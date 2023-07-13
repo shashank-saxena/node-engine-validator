b42e3261e207f0b67110ace79c3cb2efc7439291
Shashank Saxena <shashank@newput.com>
2023-07-13 17:57:18 +0530

    build(configs): setup linting and commit process

---
+++ commitlint.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ['#'],
+    },
+  },
+  rules: {
+    // 'references-empty': [2, 'never'], // Validate for issue/ticket numbers
+    'subject-case': [2, 'always', ['lower-case']],
+    'scope-enum': [2, 'always', ['wip', 'configs', 'formatting', 'code', 'deployment', 'tests']],
+  },
+};
